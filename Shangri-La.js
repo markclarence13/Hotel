283456ffd80b4df614acf8a55d28d1b379ec1cb7
markclarence13 <155933673+markclarence13@users.noreply.github.com>
2024-06-07 21:03:20 +0800

    Add files via upload

---
+++ Shangri-La.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const learnMoreButton = document.getElementById('learnMore');
+    learnMoreButton.addEventListener('click', () => {
+        alert('More information coming soon!');
+    });
+});
